f78d4a2437e55950fbafec2d83cedaa19152eb59
Zero <jrr043@latech.edu>
2019-02-18 04:44:23 -0600

    Rough nav functionality finished. Mobile for services finished.

--- js/nav.js
+++ js/nav.js
@@ -9,8 +9,25 @@ class Nav {
     }
     setup() {
         this.header.innerHTML = `S&J - ${id}`;
-        // this.button.addEventListener('click', () => {
-        // });
+        this.button.addEventListener('click', () => {
+            document.querySelector(`.${this.id.toLowerCase()}`).style.transition = '0.8s ease-in-out';
+            document.querySelector(`.header-${this.id.toLowerCase()}`).style.display = "none";
+            document.querySelector(`.${this.id.toLowerCase()}-block`).style.display = "none";
+            document.querySelector(`.${this.id.toLowerCase()}`).style.height = '80px';
+            document.querySelector(`.${this.id.toLowerCase()}`).style.margin = '0 auto';
+            document.querySelector('.expanded-nav-view').style.display = "flex";
+            this.button.src="img/nav-hamburger-close.png";
+            this.button.addEventListener('click', () => {
+                document.querySelector(`.${this.id.toLowerCase()}`).style.transition = '0.8s ease-in-out';
+                document.querySelector(`.header-${this.id.toLowerCase()}`).style.display = "flex";
+                document.querySelector(`.${this.id.toLowerCase()}-block`).style.display = "";
+                document.querySelector(`.${this.id.toLowerCase()}`).style.height = '100%';
+                document.querySelector(`.${this.id.toLowerCase()}`).style.marginBottom = '20px';
+                document.querySelector('.expanded-nav-view').style.display = "none";
+                this.button.src="img/nav-hamburger.png";
+                this.setup();
+            });
+        });
     }
 }
 
@@ -18,6 +35,6 @@ const hText = document.querySelector('h1');
 
 const id = document.querySelector('nav').dataset.title;
 
-const button = document.querySelector(`.nav-${id.toLowerCase()}`).querySelector('button');
+const button = document.querySelector(`.nav-${id.toLowerCase()}`).querySelector('img');
 
 const navbar = new Nav({ 'header' : hText, 'id' : id, 'button' : button });
