a89397b102c18c0c3a6d4e10428eae4af6605a8b
Zero <jrr043@latech.edu>
2019-02-22 12:02:32 -0600

    GSAP Anims added for nav. Really fun :)

--- js/nav.js
+++ js/nav.js
@@ -10,21 +10,29 @@ class Nav {
     setup() {
         this.header.innerHTML = `S&J - ${id}`;
         this.button.addEventListener('click', () => {
-            document.querySelector(`.${this.id.toLowerCase()}`).style.transition = '0.8s ease-in-out';
+            // document.querySelector(`.${this.id.toLowerCase()}`).style.transition = '0.8s ease-in-out';
             document.querySelector(`.header-${this.id.toLowerCase()}`).style.display = "none";
             document.querySelector(`.${this.id.toLowerCase()}-block`).style.display = "none";
             document.querySelector(`.${this.id.toLowerCase()}`).style.height = '80px';
             document.querySelector(`.${this.id.toLowerCase()}`).style.margin = '0 auto';
             document.querySelector('.expanded-nav-view').style.display = "flex";
+            TweenMax.to('.expanded-nav-view', 1.0, {opacity:'1', height: "800px"});
+            for(var i = 1; i <= 4; i++) {
+                TweenMax.to(`#link${i}`, 1.15, {opacity:'1', y:`${i*150}`, ease: Bounce.easeOut, delay:1.15});
+            }
             this.button.src="img/nav-hamburger-close.png";
             this.button.addEventListener('click', () => {
-                document.querySelector(`.${this.id.toLowerCase()}`).style.transition = '0.2s ease-in-out';
+                document.querySelector(`.${this.id.toLowerCase()}`).style.transition = '0.5s ease-in-out';
                 document.querySelector(`.header-${this.id.toLowerCase()}`).style.display = "flex";
                 document.querySelector(`.${this.id.toLowerCase()}-block`).style.display = "";
                 document.querySelector(`.${this.id.toLowerCase()}`).style.height = '100%';
                 document.querySelector(`.${this.id.toLowerCase()}`).style.marginBottom = '20px';
                 document.querySelector('.expanded-nav-view').style.display = "none";
                 this.button.src="img/nav-hamburger.png";
+                for(var i = 1; i <= 4; i++) {
+                    TweenMax.to(`#link${i}`, 0.1, {opacity:'0', y:`90px`});
+                }
+                TweenMax.to('.expanded-nav-view', 0.1, {height:'1px', opacity:'0'});
                 this.setup();
             });
         });
